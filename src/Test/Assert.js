ce6eebd8d54ed67fcf887cdce913980e4dd81578
Gary Burgess <gary.burgess@gmail.com>
2015-05-10 13:04:51 +0100

    Fix JS export name

--- src/Test/Assert.js
+++ src/Test/Assert.js
@@ -3,7 +3,7 @@
 
 // module Test.Assert
 
-exports.assert$prime = function (message) {
+exports["assert'"] = function (message) {
   return function (success) {
     return function () {
       if (!success) throw new Error(message);
